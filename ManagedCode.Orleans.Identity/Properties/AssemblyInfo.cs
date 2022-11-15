63c725153f7b1b869555ec65a6607b949ae55a84
ksemenenko <mail@ksemenenko.com>
2022-11-15 20:25:38 +0100

    wip

--- ManagedCode.Orleans.Identity/Properties/AssemblyInfo.cs
+++ ManagedCode.Orleans.Identity/Properties/AssemblyInfo.cs
@@ -4,6 +4,7 @@ using ManagedCode.Orleans.Identity;
 using Orleans;
 
 [assembly:GenerateCodeForDeclaringAssembly(typeof(Result))]
+[assembly:GenerateCodeForDeclaringAssembly(typeof(Result<>))]
 [assembly:GenerateCodeForDeclaringAssembly(typeof(SessionInfo))]
 [assembly:GenerateCodeForDeclaringAssembly(typeof(SessionModel))]
 [assembly:GenerateCodeForDeclaringAssembly(typeof(System.Security.Claims.Claim))]
