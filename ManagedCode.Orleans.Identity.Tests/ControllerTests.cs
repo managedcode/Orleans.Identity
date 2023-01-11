c3f09b0be719cbbd6a95ae1f30fbade13c1f420e
TRybina132 <tryb2516@gmail.com>
2023-01-11 18:06:41 +0200

    add class for controller tests

---
+++ ManagedCode.Orleans.Identity.Tests/ControllerTests.cs
@@ -0,0 +1,24 @@
+using ManagedCode.Orleans.Identity.Tests.Cluster;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ManagedCode.Orleans.Identity.Tests;
+
+[Collection(nameof(TestClusterApplication))]
+public class ControllerTests
+{
+    private readonly TestClusterApplication _testApp;
+    private readonly ITestOutputHelper _outputHelper;
+
+    public ControllerTests(TestClusterApplication testApp, ITestOutputHelper outputHelper)
+    {
+        _testApp = testApp;
+        _outputHelper = outputHelper;
+    }
+
+    [Fact]
+    public async Task CreateSession_ReturnOk()
+    {
+        
+    }
+}
