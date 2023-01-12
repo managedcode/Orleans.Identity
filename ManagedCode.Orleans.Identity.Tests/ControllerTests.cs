bce25b7bc1b5a8cacbce03ba5e3cc0d691b1f587
Tanya <tryb2516@gmail.com>
2023-01-12 18:12:26 +0200

    add session helper

--- ManagedCode.Orleans.Identity.Tests/ControllerTests.cs
+++ ManagedCode.Orleans.Identity.Tests/ControllerTests.cs
@@ -1,6 +1,7 @@
 using ManagedCode.Orleans.Identity.Grains.Interfaces;
 using ManagedCode.Orleans.Identity.Models;
 using ManagedCode.Orleans.Identity.Tests.Cluster;
+using ManagedCode.Orleans.Identity.Tests.Helpers;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -18,9 +19,11 @@ public class ControllerTests
         _outputHelper = outputHelper;
     }
 
-    private async Task CreateSession()
+    private async Task CreateSession(string sessionId)
     {
-
+        var createSessionModel = SessionHelper.GetTestCreateSessionModel(sessionId);
+        var sessionGrain = _testApp.Cluster.Client.GetGrain<ISessionGrain>(sessionId);
+        await sessionGrain.CreateAsync(createSessionModel);
     }
 
     [Fact]
---
+++ ManagedCode.Orleans.Identity.Tests/Helpers/SessionHelper.cs
@@ -0,0 +1,34 @@
+﻿using ManagedCode.Orleans.Identity.Models;
+using Orleans.Runtime;
+using System.Security.Claims;
+
+namespace ManagedCode.Orleans.Identity.Tests.Helpers
+{
+    public static class SessionHelper
+    {
+        public static Dictionary<string, string> SetTestClaims(string sessionId) =>
+            new Dictionary<string, string>
+            {
+                { ClaimTypes.MobilePhone, "+380500000" },
+                { ClaimTypes.Email, "test@gmail.com" },
+                { ClaimTypes.Sid, sessionId },
+                { ClaimTypes.Actor, Guid.NewGuid().ToString() },
+                { ClaimTypes.Role, "admin" }
+            };
+
+        public static CreateSessionModel GetTestCreateSessionModel(string sessionId)
+        {
+            string userId = Guid.NewGuid().ToString();
+
+            GrainId userGrainId = GrainId.Create("UserGrain", userId);
+
+            CreateSessionModel createSessionModel = new CreateSessionModel
+            {
+                UserData = SetTestClaims(sessionId),
+                UserGrainId = userGrainId
+            };
+
+            return createSessionModel;
+        }
+    }
+}
