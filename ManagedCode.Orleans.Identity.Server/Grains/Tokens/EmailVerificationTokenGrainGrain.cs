b52ca6d705ad52cfedd39d8a945c7a34121d9a3c
Tanya <tryb2516@gmail.com>
2023-01-26 03:27:55 +0200

    add  timer and reminder

--- ManagedCode.Orleans.Identity.Server/Grains/Tokens/EmailVerificationTokenGrainGrain.cs
+++ ManagedCode.Orleans.Identity.Server/Grains/Tokens/EmailVerificationTokenGrainGrain.cs
@@ -5,12 +5,13 @@ using ManagedCode.Orleans.Identity.Constants;
 using ManagedCode.Orleans.Identity.Extensions;
 using ManagedCode.Orleans.Identity.Interfaces.TokenGrains;
 using ManagedCode.Orleans.Identity.Models;
+using ManagedCode.Orleans.Identity.Server.Constants;
 using Orleans;
 using Orleans.Runtime;
 
 namespace ManagedCode.Orleans.Identity.Server.Grains.Tokens;
 
-public class EmailVerificationTokenGrainGrain : Grain, IEmailVerificationTokenGrain
+public class EmailVerificationTokenGrainGrain : Grain, IEmailVerificationTokenGrain, IRemindable
 {
     private readonly IPersistentState<TokenModel> _tokenState;
 
@@ -21,19 +22,22 @@ public class EmailVerificationTokenGrainGrain : Grain, IEmailVerificationTokenGr
         _tokenState = tokenState;
     }
     
-    
+    private async Task OnTimerTicked(object args)
+    {
+        if (_tokenState.RecordExists is false)
+            return;
+
+        _tokenState.State.IsActive = false;
+
+        await _tokenState.WriteStateAsync();
+    }
     
     public async ValueTask<Result> CreateAsync(CreateTokenModel createModel)
     {
-        // TODO: add to the method description that if token lifetime less than 1 minute 
+        // TODO: add to the method description that if token lifetime less than 1 minute, timer with period 1 minute will be registered 
         if (createModel.IsModelValid() is false)
         {
             return Result.Fail();
-        }
-
-        if (createModel.Lifetime < TimeSpan.FromMinutes(1))
-        {
-                  
         }
         
         _tokenState.State = new TokenModel
@@ -42,9 +46,18 @@ public class EmailVerificationTokenGrainGrain : Grain, IEmailVerificationTokenGr
             Lifetime = createModel.Lifetime,
             UserGrainId = createModel.UserGrainId,
             Value = createModel.Value,
-        };
+        }; 
 
         await _tokenState.WriteStateAsync();
+
+        if (createModel.Lifetime < TimeSpan.FromMinutes(1))
+        {
+            RegisterTimer(OnTimerTicked, null, TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1));
+        }
+        else
+        {
+            await this.RegisterOrUpdateReminder(TokenGrainConstants.EMAIL_VERIFICATION_TOKEN_REMINDER_NAME, _tokenState.State.Lifetime, _tokenState.State.Lifetime);
+        }
 
         return Result.Succeed();
     }
@@ -52,5 +65,17 @@ public class EmailVerificationTokenGrainGrain : Grain, IEmailVerificationTokenGr
     public ValueTask<Result> VerifyAsync()
     {
         throw new System.NotImplementedException();
+    }
+
+    public async Task ReceiveReminder(string reminderName, TickStatus status)
+    {
+        if (_tokenState.RecordExists is false)
+            return;
+
+        if (reminderName == TokenGrainConstants.EMAIL_VERIFICATION_TOKEN_REMINDER_NAME)
+        {
+            _tokenState.State.IsActive = false;
+            await _tokenState.WriteStateAsync();
+        }
     }
 }
