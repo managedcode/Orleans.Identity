63472e70b653f10da8cee596076e044ad8e0ac6d
TRybina132 <tryb2516@gmail.com>
2023-01-27 00:14:14 +0200

    add MagicLinkGrain interface

---
+++ ManagedCode.Orleans.Identity/Interfaces/TokenGrains/IMagicLinkTokenGrain.cs
@@ -0,0 +1,9 @@
+using System.Threading.Tasks;
+using ManagedCode.Communication;
+
+namespace ManagedCode.Orleans.Identity.Interfaces.TokenGrains;
+
+public interface IMagicLinkTokenGrain : IBaseTokenGrain
+{
+    ValueTask<Result> VerifyAsync();
+}
