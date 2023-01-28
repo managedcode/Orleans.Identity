38af727bea6ebeda4ab8039b9eca393c805d1f79
TRybina132 <tryb2516@gmail.com>
2023-01-28 17:34:41 +0200

    add tests for get token method

--- ManagedCode.Orleans.Identity.Tests/TokenGrainTests/BaseTokenGrainTests.cs
+++ ManagedCode.Orleans.Identity.Tests/TokenGrainTests/BaseTokenGrainTests.cs
@@ -76,12 +76,15 @@ namespace ManagedCode.Orleans.Identity.Tests.TokenGrainTests
         [Fact]
         public virtual async Task CreateToken_WhenValueIsValid_ReturnSuccess()
         {
+            // Arrange
             var createTokenModel = GenerateCreateTestTokenModel();
-
             var tokenGrain = _testApp.Cluster.Client.GetGrain<TGrain>(createTokenModel.Value);
+            
+            // Act
             var result = await tokenGrain.CreateAsync(createTokenModel);
+            
+            // Assert
             var token = await tokenGrain.GetTokenAsync();
-
             result.IsSuccess.Should().BeTrue();
             token.IsSuccess.Should().BeTrue();
             token.Value.Should().NotBeNull();
@@ -91,12 +94,15 @@ namespace ManagedCode.Orleans.Identity.Tests.TokenGrainTests
         [Fact]
         public virtual async Task CreateToken_WhenTokensLifetimeLessThanMinute_ReturnSuccess()
         {
+            // Arrange
             var createTokenModel = GenerateCreateTestTokenModel(TimeSpan.FromSeconds(30));
-
             var tokenGrain = _testApp.Cluster.Client.GetGrain<TGrain>(createTokenModel.Value);
+            
+            // Act
             var result = await tokenGrain.CreateAsync(createTokenModel);
+            
+            // Assert
             var token = await tokenGrain.GetTokenAsync();
-
             result.IsSuccess.Should().BeTrue();
             token.IsSuccess.Should().BeTrue();
             token.Value.Should().NotBeNull();
@@ -106,47 +112,94 @@ namespace ManagedCode.Orleans.Identity.Tests.TokenGrainTests
         [Fact]
         public virtual async Task CreateToken_WhenTokensValueIsEmpty_ReturnFail()
         {
+            // Arrange
             var createTokenModel = GenerateCreateTestTokenModel(string.Empty);
-
             var tokenGrain = _testApp.Cluster.Client.GetGrain<TGrain>(createTokenModel.Value);
+
+            // Act
             var result = await tokenGrain.CreateAsync(createTokenModel);
 
+            // Assert
             result.IsFailed.Should().BeTrue();
         }
         
         [Fact]
         public virtual async Task CreateToken_WhenTokensValueIsNull_ReturnFail()
         {
+            // Arrange
             var createTokenModel = GenerateCreateTestTokenModel(null);
-
             var tokenGrain = _testApp.Cluster.Client.GetGrain<TGrain>(createTokenModel.Value);
+            
+            // Act
             var result = await tokenGrain.CreateAsync(createTokenModel);
 
+            // Assert
             result.IsFailed.Should().BeTrue();
         }
         
         [Fact]
         public virtual async Task CreateToken_WhenTokensValueIsWhiteSpace_ReturnFail()
         {
+            // Arrange
             var createTokenModel = GenerateCreateTestTokenModel(" ");
-
             var tokenGrain = _testApp.Cluster.Client.GetGrain<TGrain>(createTokenModel.Value);
+            
+            // Act
             var result = await tokenGrain.CreateAsync(createTokenModel);
 
+            // Assert
             result.IsFailed.Should().BeTrue();
         }
 
         [Fact]
         public virtual async Task CreateToken_WhenTokensLifetimeIsZero_ReturnFail()
         {
+            // Arrange
             var createTokenModel = GenerateCreateTestTokenModel(TimeSpan.Zero);
-
             var tokenGrain = _testApp.Cluster.Client.GetGrain<TGrain>(createTokenModel.Value);
+            
+            // Act
             var result = await tokenGrain.CreateAsync(createTokenModel);
 
+            // Assert
             result.IsFailed.Should().BeTrue();
         }
 
         #endregion
+
+        #region GetToken
+
+        [Fact]
+        public async Task GetToken_WhenTokenExists_ReturnSuccess()
+        {
+            // Arrange
+            var createTokenModel = GenerateCreateTestTokenModel();
+            var tokenGrain = _testApp.Cluster.Client.GetGrain<TGrain>(createTokenModel.Value);
+            await tokenGrain.CreateAsync(createTokenModel);
+            
+            // Act
+            var result = await tokenGrain.GetTokenAsync();
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Value.Value.Should().Be(createTokenModel.Value);
+        }
+
+        [Fact]
+        public async Task GetToken_WhenTokenDoesntExist_ReturnFail()
+        {
+            // Arrange
+            var randValue = Guid.NewGuid().ToString();
+            var tokenGrain = _testApp.Cluster.Client.GetGrain<TGrain>(randValue);
+            
+            // Act
+            var result = await tokenGrain.GetTokenAsync();
+
+            // Assert
+            result.IsFailed.Should().BeTrue();
+        }
+
+        #endregion
+        
     }
 }
