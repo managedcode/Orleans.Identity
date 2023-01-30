ff9933b00591dfb6f2368ac8919af0b50e08cb75
Tanya <tryb2516@gmail.com>
2023-01-30 10:53:55 +0200

    add base interface for user grain

---
+++ ManagedCode.Orleans.Identity/Interfaces/UserGrain/IEmailVerificationUserGrain.cs
@@ -0,0 +1,12 @@
+﻿using ManagedCode.Communication;
+using System.Threading.Tasks;
+
+namespace ManagedCode.Orleans.Identity.Interfaces.UserGrain
+{
+    public interface IEmailVerificationUserGrain
+    {
+        ValueTask<Result> EmailVerificationTokenExpiredAsync();
+
+        ValueTask<Result> EmailVerificationTokenValidAsync();
+    }
+}
