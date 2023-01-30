d08ad992840d723582058051578b3dbbb63572b1
Tanya <tryb2516@gmail.com>
2023-01-30 10:48:06 +0200

    add user grain interface

---
+++ ManagedCode.Orleans.Identity/Interfaces/UserGrain/IEmailVerificationUserGrain.cs
@@ -0,0 +1,9 @@
+﻿using Orleans;
+
+namespace ManagedCode.Orleans.Identity.Interfaces.UserGrain
+{
+    public interface IEmailVerificationUserGrain : IGrainWithStringKey
+    {
+        ValueTask<>
+    }
+}
