805b2b70c82d3ae792a7dda31e389aa8a25376d1
Tanya <tryb2516@gmail.com>
2023-01-31 15:52:21 +0200

    add base class for user grain tests

---
+++ ManagedCode.Orleans.Identity.Tests/TokenGrainTests/UserGrainTests/BaseUserGrainsTests.cs
@@ -0,0 +1,37 @@
+﻿using ManagedCode.Orleans.Identity.Interfaces.TokenGrains;
+using ManagedCode.Orleans.Identity.Tests.Cluster;
+using ManagedCode.Orleans.Identity.Tests.Helpers;
+using Xunit;
+
+namespace ManagedCode.Orleans.Identity.Tests.TokenGrainTests.UserGrainTests
+{
+    [Collection(nameof(TestClusterApplication))]
+    public class BaseUserGrainsTests<TokenGrain>
+        where TokenGrain : IBaseTokenGrain
+    {
+        #region Create TokenGrain methods
+
+        protected async Task<TokenGrain> CreateTokenAsync()
+        {
+            var createTokenModel = TokenHelper.GenerateCreateTestTokenModel();
+        }
+
+        protected async Task<TokenGrain> CreateTokenAsync(string tokenValue)
+        {
+            return null;
+        }
+
+        protected async Task<TokenGrain> CreateTokenAsync(string tokenValue, TimeSpan timeSpan)
+        {
+            return null;
+        }
+
+        protected async Task<TokenGrain> CreateTokenAsync(TimeSpan timeSpan)
+        {
+            return null;
+        }
+
+        #endregion
+    }
+
+}
