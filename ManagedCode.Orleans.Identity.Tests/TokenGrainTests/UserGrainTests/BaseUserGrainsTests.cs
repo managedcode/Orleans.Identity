8fecc31811c02d897205e8641a39e5efd77c35ca
TRybina132 <tryb2516@gmail.com>
2023-01-31 22:37:41 +0200

    add methods for creating tokens and receiving token grain reference

--- ManagedCode.Orleans.Identity.Tests/TokenGrainTests/UserGrainTests/BaseUserGrainsTests.cs
+++ ManagedCode.Orleans.Identity.Tests/TokenGrainTests/UserGrainTests/BaseUserGrainsTests.cs
@@ -1,34 +1,60 @@
 ﻿using ManagedCode.Orleans.Identity.Interfaces.TokenGrains;
+using ManagedCode.Orleans.Identity.Models;
 using ManagedCode.Orleans.Identity.Tests.Cluster;
 using ManagedCode.Orleans.Identity.Tests.Helpers;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace ManagedCode.Orleans.Identity.Tests.TokenGrainTests.UserGrainTests
 {
     [Collection(nameof(TestClusterApplication))]
-    public class BaseUserGrainsTests<TokenGrain>
-        where TokenGrain : IBaseTokenGrain
+    public class BaseUserGrainsTests<TTokenGrain>
+        where TTokenGrain : IBaseTokenGrain
     {
+        protected readonly ITestOutputHelper _outputHelper;
+        protected readonly TestClusterApplication _testApp;
+
+        public BaseUserGrainsTests(ITestOutputHelper outputHelper, TestClusterApplication testApp)
+        {
+            _outputHelper = outputHelper;
+            _testApp = testApp;
+        }
+
         #region Create TokenGrain methods
 
-        protected async Task<TokenGrain> CreateTokenAsync()
+        private async ValueTask<TTokenGrain> CreateAndGetTokenGrainAsync(CreateTokenModel createTokenModel)
+        {
+            var tokenGrain = _testApp.Cluster.Client.GetGrain<TTokenGrain>(createTokenModel.Value);
+            await tokenGrain.CreateAsync(createTokenModel);
+            return tokenGrain;
+        }
+
+        protected async Task<TTokenGrain> CreateTokenAsync()
         {
             var createTokenModel = TokenHelper.GenerateCreateTestTokenModel();
+            var tokenGrain = await CreateAndGetTokenGrainAsync(createTokenModel);
+            return tokenGrain;
+        }
+        
+        protected async Task<TTokenGrain> CreateTokenAsync(string tokenValue)
+        {
+            var createTokenModel = TokenHelper.GenerateCreateTestTokenModel(tokenValue);
+            var tokenGrain = await CreateAndGetTokenGrainAsync(createTokenModel);
+            return tokenGrain;
         }
 
-        protected async Task<TokenGrain> CreateTokenAsync(string tokenValue)
+        protected async Task<TTokenGrain> CreateTokenAsync(string tokenValue, TimeSpan timeSpan)
         {
-            return null;
+            var createTokenModel = TokenHelper.GenerateCreateTestTokenModel(tokenValue, timeSpan);
+            var tokenGrain = await CreateAndGetTokenGrainAsync(createTokenModel);
+            return tokenGrain;
         }
 
-        protected async Task<TokenGrain> CreateTokenAsync(string tokenValue, TimeSpan timeSpan)
+        protected async Task<TTokenGrain> CreateTokenAsync(TimeSpan timeSpan)
         {
-            return null;
-        }
-
-        protected async Task<TokenGrain> CreateTokenAsync(TimeSpan timeSpan)
-        {
-            return null;
+            var createTokenModel = TokenHelper.GenerateCreateTestTokenModel(timeSpan);
+            var tokenGrain = await CreateAndGetTokenGrainAsync(createTokenModel);
+            return tokenGrain;
         }
 
         #endregion
