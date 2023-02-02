2837ce52aa3696c6f5fdbd0372e034a30a67e3da
Tanya <tryb2516@gmail.com>
2023-02-02 19:53:21 +0200

    fix test user grains

---
+++ ManagedCode.Orleans.Identity.Tests/Cluster/Grains/UserGrains/CodeUserGrain.cs
@@ -0,0 +1,28 @@
+﻿using ManagedCode.Communication;
+using ManagedCode.Orleans.Identity.Tests.Cluster.Grains.Interfaces.UserGrains;
+
+namespace ManagedCode.Orleans.Identity.Tests.Cluster.Grains.UserGrains;
+
+public class CodeUserGrain : BaseTestUserGrain, ICodeUserGrain
+{
+    public ValueTask<Result> CodeVerificationTokenExpiredAsync(string token)
+    {
+        _tokenValue = token;
+        _tokenExpired = true;
+        return Result.Succeed().AsValueTask();
+    }
+
+    public ValueTask<Result> CodeVerificationTokenInvalidAsync(string token)
+    {
+        _tokenValue = token;
+        _tokenInvalid = true;
+        return Result.Succeed().AsValueTask();
+    }
+
+    public ValueTask<Result> CodeVerificationTokenValidAsync(string token)
+    {
+        _tokenValue = token;
+        _tokenValid = true;
+        return Result.Succeed().AsValueTask();
+    }
+}
